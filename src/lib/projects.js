2532ddec9ae97939e78bffba2006039e8903e54f
chasscepts <francischassobetta@gmail.com>
2021-06-22 16:03:01 +0100

    Correct grammatical errors

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -77,7 +77,7 @@ const detailsPage = (() => {
 const all = [
   new Project(
     'Marked Field',
-    'A Markdown Editor. It uses Electron for graphical User Interface.',
+    'A split view Markdown Editor. The preview is updated in real life as the user changes the markdown.',
     'Markedfield is a Markdown Editor with a split view for editing the markdown and previewing the HTML. The preview automatically updates as the markdown changes. For parsing markdown, Marked Field uses <a href="https://github.com/markedjs/marked" target="_blank">Marked</a>. It is a standalone application that uses Electron for the UI.',
     marked,
     ['JavaScript', 'Electron', 'HTML'],
@@ -87,7 +87,7 @@ const all = [
   ),
   new Project(
     'Handy Commands',
-    'A Visual Studio Extension for organising frequently used command line commands.',
+    'A Visual Studio Code extension for organizing frequently used commands.',
     'Handy Commands is a Visual Studio Code extension for organizing commands that we frequently run on the command line. Sometimes we forget that command we want to run, and we will leave our development environment to search for it. That is not productive. With Handy Commands, you organize them into groups of related commands and run them with the click of the mouse. If some part of the command changes from run to run, you can paste and edit it in the terminal. If you no longer frequently use a command, you can delete it. And by organizing them into groups, they will not clutter your workspace. From v0.0.2, Handy Commands automatically imports the scripts defined in package.json and puts them in their group (Npm Scripts). It watches changes to package.json and updates this group as the script changes.',
     handyCommands,
     ['TypeScript', 'VSCode API'],
@@ -109,7 +109,7 @@ const all = [
   ),
   new Project(
     'Flights',
-    'A command-line utility written in Ruby to scrape the internet for the day or future flight deals.',
+    'A command-line utility application, written in Ruby and used to curate flight deals from the internet.',
     'Flights is a command-line utility application written in Ruby to scrape the internet for the day or future flight deals. It helps you avoid browsing countless websites for flight information by providing a simple command-line interface that will curate the best deals for you.',
     flights,
     ['Ruby'],
@@ -119,7 +119,7 @@ const all = [
   ),
   new Project(
     'Chass Corner',
-    'A Lifestyle Blog where we discuss Family, Sports, Entertainment and Politics.',
+    'An exclusive lifestyle blog where we discuss Family, Sports, Entertainment, and Politics.',
     "Chass corner is a lifestyle blog where we discuss sports, politics, entertainment, family, and just about anything else. As a member of chass corner, You can contribute articles, read articles, and vote for your favorites. Just drop by 'The Corner' anytime, and You will find something there that will interest you.",
     chassCorner,
     ['Rails', 'JavaScript', 'SCSS'],
@@ -130,7 +130,7 @@ const all = [
   ),
   new Project(
     'Weather App',
-    'An App to retrieve and display weather information from the OpenWeatherMap API.',
+    'An App to pull and display weather information from the OpenWeatherMap API.',
     'Weather-app is an app to pull and display weather information from the OpenWeatherMap API. A user can search for Weather information by entering a city in the provided field. For quick searches, the App has a button that allows the user to search the information for the current city. The background of the page changes to reflect the current weather information. The design is simple and efficient.',
     weatherApp,
     ['JavaScript', 'Webpack', 'SCSS'],
